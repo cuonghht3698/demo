842403f2c2f0435e6566023490354f4ac790076e
cuong <cuonghht3698@gmail.com>
2020-09-11 00:03:11 +0700

    Giao diện

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class FormComponent implements OnInit {
 
   constructor() { }
-
+  pathImg:string='';
   ngOnInit(): void {
-  }
 
+
+  }
+  ChoseFile(){
+   document.getElementById('file').click();
+   console.log(this.pathImg);
+
+  }
 }
